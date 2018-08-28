d86ea5439e8a82ec291e728b31bb3b7da6997931
Yuguang Duan <ariaduan@pku.edu.cn>
2018-08-29 00:17:22 +0800

    Update split.py

--- scripts/split.py
+++ scripts/split.py
@@ -15,7 +15,8 @@ corpus_f = args.input.open("rb")
 out_dir = args.out_dir
 dependency = args.dependency
 language = args.language
-out_files = {"train": (out_dir / "v_a_lists_{}_{}.train".format(dependency, language)).open("w"), "test": (out_dir / "v_a_lists_{}_{}.test".format(dependency, language)).open("w")}
+out_list_files = {"train": (out_dir / "v_a_lists_{}_{}.train".format(dependency, language)).open("w"), "test": (out_dir / "v_a_lists_{}_{}.test".format(dependency, language)).open("w")}
+out_pair_files = {"train": (out_dir / "v_a_pairs_{}_{}.train".format(dependency, language)).open("w"), "test": (out_dir / "v_a_pairs_{}_{}.test".format(dependency, language)).open("w")}
 
 verbs = {}
 args = {}
@@ -48,21 +49,25 @@ for i in lstkey:
 cnt = 0
 i = 0
 while i < len(lst):
-	out_files["train"].write(lst[i] + '\t')
+	out_list_files["train"].write(lst[i] + '\t')
 	for j in args[lst[i]]:
-		out_files["train"].write(j + '\t')
-	out_files["train"].write('\n')
+		out_list_files["train"].write(j + '\t')
+		out_pair_files["train"].write(lst[i] + '\t' + j + '\n')
+	out_list_files["train"].write('\n')
 	cnt += 1
 	i += 1
 	if cnt == 4:
-		out_files["test"].write(lst[i] + '\t')
+		out_list_files["test"].write(lst[i] + '\t')
 		for j in args[lst[i]]:
-			out_files["test"].write(j + '\t')
-		out_files["test"].write('\n')
+			out_list_files["test"].write(j + '\t')
+			out_pair_files["test"].write(lst[i] + '\t' + j + '\n')
+		out_list_files["test"].write('\n')
 		i += 1
 		cnt = 0
 
 inp_f.close()
 corpus_f.close()
-out_files["train"].close()
-out_files["test"].close()
+out_list_files["train"].close()
+out_list_files["test"].close()
+out_pair_files["train"].close()
+out_pair_files["test"].close()
